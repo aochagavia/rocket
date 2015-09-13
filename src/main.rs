cfd6d8e3a04938fe47c4ff7c8d4f6b94e577d59f
Adolfo Ochagavía <aochagavia92@gmail.com>
2015-09-13 21:26:42 +0200

    Update dependencies and add sampling fallback

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,30 @@ mod traits;
 
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event::{Event, Events, EventLoop, RenderEvent};
-use piston::input::{Button, Input};
+use piston::event_loop::{Events, EventLoop};
+use piston::input::{Button, Event, Input, RenderEvent};
 use piston::window::WindowSettings;
 
 use drawing::Size;
 use game::Game;
 
+const OPEN_GL: OpenGL = OpenGL::V2_1;
+
+// Returns a result containing a GlutinWindow or an error if the window
+// settings are not supported
+fn try_create_window(samples: u8) -> Result<GlutinWindow, String> {
+    WindowSettings::new("Rocket", [1024, 600])
+        .exit_on_esc(true)
+        .opengl(OPEN_GL)
+        .samples(samples)
+        .build()
+}
+
 fn main() {
-    // Initialization stuff
-    let opengl = OpenGL::_2_1;
+    // Create a window with a sampling of 8 or fall back to 0
+    let window = try_create_window(8).or_else(|_| try_create_window(0)).unwrap();
 
-    let window: GlutinWindow =
-        WindowSettings::new("Rocket", [1024, 600])
-            .exit_on_esc(true)
-            .opengl(opengl)
-            .samples(8)
-            .into();
-
-    let mut gl = GlGraphics::new(opengl);
+    let mut gl = GlGraphics::new(OPEN_GL);
 
     // The game object
     let mut game = Game::new(Size::new(1024.0, 600.0));
